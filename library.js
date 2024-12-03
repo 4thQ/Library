71fee52a86d7ebfc90e6dffab468dbfe7c1ef9de
Nirajan Gurung <diorrg@outlook.com>
2024-12-03 18:57:20 +0000

    front updated

--- library.js
+++ library.js
@@ -7,22 +7,48 @@ function Book(author, title, pages, read) {
   this.read = read;
 }
 
-const book1 = new Book("N Gurung", "GoodNight", 10, "read");
-const book2 = new Book("H Jan", "GoodMorning", 20, "read");
+const book1 = new Book("N Gurung", "GoodNight", 10, "Read");
+const book2 = new Book("H Jan", "GoodMorning", 20, "Not Read");
+const book3 = new Book("H Jan", "GoodMorning", 20, "Not Read");
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary({});
 
 function addBookToLibrary(bookObject) {
   myLibrary.push(bookObject);
   console.log(myLibrary);
 }
 
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-
 function displayBook(myLibrary) {
   for (const book of myLibrary) {
+    const container = document.querySelector(".container");
     const div = document.createElement("div");
-    div.innerHTML = book.title;
-    document.body.appendChild(div);
+
+    container.appendChild(div);
+
+    const p = document.createElement("p");
+    p.textContent = "Title: " + book.title;
+
+    const p1 = document.createElement("p");
+    p1.textContent = "Author: " + book.author;
+
+    const p2 = document.createElement("p");
+    p2.textContent = "Pages:" + book.pages;
+
+    const p3 = document.createElement("p");
+    p3.textContent = "Status:" + book.read;
+
+    div.append(p, p1, p2, p3);
+
+    const statusButton = document.createElement("button");
+    statusButton.textContent = "Status";
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+
+    div.appendChild(statusButton);
+    div.appendChild(deleteButton);
   }
 }
 
