4751353b2f5e5ed799d533a3d5ff5a578afb5b90
Nirajan Gurung <diorrg@outlook.com>
2024-12-03 00:30:08 +0000

    updated 2

--- library.js
+++ library.js
@@ -1,15 +1,41 @@
-const myLibrary = ["Harri Putor", "Tingled", "Twolight"];
+const myLibrary = [
+  { name: "J Miguel", title: "Remeber me", pages: 102, read: "not read" },
+  { name: "G Nirajan", title: "Masterjan", pages: 0, read: "reading" },
+];
 
 function Book() {}
 
 function addBookToLibrary() {}
 
 function displayBooks(myLibrary) {
-  for (i = 0; i < myLibrary.length; i++) {
+  for (let book of myLibrary) {
     const container = document.querySelector(".container");
-    div = document.createElement("div");
-    div.innerHTML = myLibrary[i];
+    const div = document.createElement("div");
     container.appendChild(div);
+
+    const p = document.createElement("p");
+    p.textContent = "Author: " + book.name;
+    div.appendChild(p);
+
+    const p1 = document.createElement("p");
+    p1.textContent = "Title: " + book.title;
+    div.appendChild(p1);
+
+    const p2 = document.createElement("p");
+    p2.textContent = "Pages: " + book.pages;
+    div.appendChild(p2);
+
+    const p3 = document.createElement("p");
+    p3.textContent = "Read: " + book.read;
+    div.appendChild(p3);
+
+    readStatusButton = document.createElement("button");
+    readStatusButton.textContent = "Status";
+    div.appendChild(readStatusButton);
+
+    removeButton = document.createElement("button");
+    removeButton.textContent = "Remove!";
+    div.appendChild(removeButton);
   }
 }
 
@@ -17,7 +43,22 @@ displayBooks(myLibrary);
 
 const dialog = document.querySelector("dialog");
 const addButton = document.querySelector(".addButton");
+const closeButton = document.querySelector(".closeButton");
 
 addButton.onclick = () => {
   dialog.showModal();
 };
+
+closeButton.onclick = () => {
+  dialog.close();
+  console.log("Closed");
+};
+
+const bookForm = document.querySelector(".bookForm");
+bookForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const name = document.querySelector('[name="name"]').value;
+});
+// const name = document.querySelector('[name="name"]').value;
+//   myLibrary.push(name);
+//   console.log(myLibrary);
