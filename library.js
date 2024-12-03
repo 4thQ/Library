0e147e1abc7b9c77759425f47efbc6bfe3ebbdd9
Nirajan Gurung <diorrg@outlook.com>
2024-12-03 22:34:31 +0000

    finshed logic of JS, css needed

--- library.js
+++ library.js
@@ -1,55 +1,83 @@
 const myLibrary = [];
 
-function Book(author, title, pages, read) {
+function Book(title, author, pages, read) {
   this.author = author;
   this.title = title;
   this.pages = pages;
   this.read = read;
 }
 
-const book1 = new Book("N Gurung", "GoodNight", 10, "Read");
-const book2 = new Book("H Jan", "GoodMorning", 20, "Not Read");
-const book3 = new Book("H Jan", "GoodMorning", 20, "Not Read");
-
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-addBookToLibrary({});
-
-function addBookToLibrary(bookObject) {
-  myLibrary.push(bookObject);
-  console.log(myLibrary);
+function addBookToLibrary(title, author, pages, read) {
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
 }
 
 function displayBook(myLibrary) {
-  for (const book of myLibrary) {
-    const container = document.querySelector(".container");
-    const div = document.createElement("div");
+  const book = myLibrary.pop();
 
-    container.appendChild(div);
+  const container = document.querySelector(".container");
+  const div = document.createElement("div");
 
-    const p = document.createElement("p");
-    p.textContent = "Title: " + book.title;
+  container.appendChild(div);
 
-    const p1 = document.createElement("p");
-    p1.textContent = "Author: " + book.author;
+  const p = document.createElement("p");
+  p.textContent = "Title: " + book.title;
 
-    const p2 = document.createElement("p");
-    p2.textContent = "Pages:" + book.pages;
+  const p1 = document.createElement("p");
+  p1.textContent = "Author: " + book.author;
 
-    const p3 = document.createElement("p");
-    p3.textContent = "Status:" + book.read;
+  const p2 = document.createElement("p");
+  p2.textContent = "Pages:" + book.pages;
 
-    div.append(p, p1, p2, p3);
+  const p3 = document.createElement("p");
+  p3.textContent = "Status:" + book.read;
 
-    const statusButton = document.createElement("button");
-    statusButton.textContent = "Status";
+  div.append(p, p1, p2, p3);
 
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
+  const statusButton = document.createElement("button");
+  statusButton.textContent = "Status";
+  statusButton.addEventListener("click", (e) => {
+    if (p3.innerText === "Status:Read") {
+      p3.innerText = "Status:Not Read";
+    } else {
+      p3.innerText = "Status:Read";
+    }
+  });
 
-    div.appendChild(statusButton);
-    div.appendChild(deleteButton);
-  }
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.addEventListener("click", (e) => {
+    container.removeChild(div);
+  });
+
+  div.appendChild(statusButton);
+  div.appendChild(deleteButton);
 }
 
-displayBook(myLibrary);
+const addButton = document.querySelector(".addButton");
+const dialog = document.querySelector("dialog");
+const submitButton = document.querySelector(".submitButton");
+const closeButton = document.querySelector(".closeButton");
+
+addButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  let form = new FormData(document.querySelector("form"));
+  let userTitle = form.get("book-title");
+  let userAuthor = form.get("author");
+  let userPages = form.get("pages");
+  let userStatus = form.get("status");
+
+  addBookToLibrary(userTitle, userAuthor, userPages, userStatus);
+  displayBook(myLibrary);
+  dialog.close();
+  document.querySelector("form").reset();
+});
+
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
